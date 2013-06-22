5770c0b6befccd198558ea6737bf9af084080275
Alex Gibson <a.gibson@me.com>
2013-06-22 21:40:05 +0100

    Add site.js in head for pre-css hooks

---
+++ _site/js/site.js
@@ -0,0 +1,5 @@
+(function () {
+    var doc = document.documentElement;
+    // Add class to reflect javascript availability for CSS
+    doc.className = doc.className.replace(/\bno-js\b/, 'js');
+})();
