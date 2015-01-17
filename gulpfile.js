2860877a67302e579c76f883f2ffb8f8a583f775
Alex Gibson <a.gibson@me.com>
2015-01-17 09:27:13 +0000

    Add Jekyll build task for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,16 @@ var options = {
     branch: 'master'
 };
 
-gulp.task('deploy', function () {
+gulp.task('deploy', ['jekyll:build'], function () {
     return gulp.src('./_site/**/*')
         .pipe(deploy(options));
+});
+
+gulp.task('jekyll:build', function (gulpCallBack){
+    var spawn = require('child_process').spawn;
+    var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+
+    jekyll.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+    });
 });
