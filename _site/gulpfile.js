3a707a42aa851a99f11969dd4cf022b4d87adaea
Alex Gibson <a.gibson@me.com>
2015-01-17 09:14:20 +0000

    Add Gulp config for deployment

---
+++ _site/gulpfile.js
@@ -0,0 +1,10 @@
+var deploy = require('gulp-gh-pages');
+
+var options = {
+    branch: 'master'
+};
+
+gulp.task('deploy', function () {
+    return gulp.src('./_site/**/*')
+        .pipe(deploy(options));
+});
