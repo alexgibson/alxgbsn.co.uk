263e1d05d9bf73cb9a090106e8f61622101f4d16
Alex Gibson <a.gibson@me.com>
2015-03-21 09:03:31 +0000

    Remove all JS in <head>

--- js/site.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    var doc = document.documentElement;
-    // Add class to reflect javascript availability for CSS
-    doc.className = doc.className.replace(/\bno-js\b/, 'js');
-})();
