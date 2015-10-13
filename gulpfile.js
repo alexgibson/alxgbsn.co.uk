4d508b0ef86a5ded0f2b781d9ac45d3d2efad193
Alex Gibson <a.gibson@me.com>
2015-10-13 22:08:57 +0100

    Use ES6 in Gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
 'use strict';
 
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var compass = require('gulp-compass');
-var jshint = require('gulp-jshint');
-var htmlmin = require('gulp-html-minifier');
-var del = require('del');
-var runSequence = require('run-sequence');
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const compass = require('gulp-compass');
+const jshint = require('gulp-jshint');
+const htmlmin = require('gulp-html-minifier');
+const del = require('del');
+const runSequence = require('run-sequence');
 
-var options = {
+let options = {
     branch: 'master'
 };
 
-gulp.task('jekyll:build', function (gulpCallBack){
-    var spawn = require('child_process').spawn;
-    var jekyll = spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'});
+gulp.task('jekyll:build', (gulpCallBack) => {
+    let spawn = require('child_process').spawn;
+    let jekyll = spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'});
 
-    jekyll.on('exit', function(code) {
+    jekyll.on('exit', (code) => {
         gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
     });
 });
 
-gulp.task('site:build', function(callback) {
+gulp.task('site:build', (callback) => {
     runSequence('clean', ['compass:compile', 'js:lint'], 'jekyll:build', 'minify:html', callback);
 });
 
-gulp.task('compass:compile', function() {
+gulp.task('compass:compile', () => {
     return gulp.src('./sass/*.scss')
         .pipe(compass({
             config_file: './config.rb',
@@ -35,27 +35,27 @@ gulp.task('compass:compile', function() {
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('minify:html', function() {
+gulp.task('minify:html', () => {
   gulp.src('./_site/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true }))
     .pipe(gulp.dest('./_site'));
 });
 
-gulp.task('js:lint', function() {
+gulp.task('js:lint', () => {
     return gulp.src(['./js/*.js', '!./js/lib/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', () => {
     return del(['_site/**']);
 });
 
-gulp.task('default', function () {
-    watch('./sass/*.scss', function () {
+gulp.task('default', () => {
+    watch('./sass/*.scss', () => {
         gulp.start('compass:compile');
     });
-    watch('./js/*.js', function () {
+    watch('./js/*.js', () => {
         gulp.start('js:lint');
     });
 });
