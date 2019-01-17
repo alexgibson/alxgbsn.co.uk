0324424e5836e04a150e63e328bc4a8fa50a7615
Alex Gibson <alexgibson@users.noreply.github.com>
2019-01-17 10:11:09 +0000

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '20190110150908';
+const version = '20190117101106';
 const cacheName = `static::${version}`;
 
 
@@ -6,7 +6,7 @@ function updateStaticCache() {
     return caches.open(cacheName).then(cache => {
         return cache.addAll([
             
-            '/assets/styles-4404055533b147bc89a42be048f0ca2585ff487e5ad99f664a8dbcf0dba099d5.css',
+            '/assets/styles-30d1c44e71e17534b979fe0186f6fea940314e407433c315de8445e874c6fb9a.css',
             '/assets/main-7fde6c85d9901972362b6794d34921bb0f4cca13d845a8378d3358f037ce2144.js',
             '/assets/avatar-180-a1b105f177ccb815e56ac246a8958ead865bf75115d4e851fab3172195d9e903.png',
             '/offline/'
