7379418a9ba3d409ba69dae16ca831ab5b75635c
Alex Gibson <alexgibson@users.noreply.github.com>
2019-01-21 18:04:05 +0000

    Add theme toggle

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,20 +1,50 @@
-(function() {
-    'use strict';
+function registerServiceWorker() {
 
-    function registerServiceWorker() {
-
-        if (!navigator.serviceWorker) {
-            return;
-        }
-
-        navigator.serviceWorker.register('/sw.js')
-            .then(() => {
-                console.log('Service Worker: registered'); // eslint-disable-line no-console
-            }).catch(err => {
-                console.log('Service Worker: registration failed ', err); // eslint-disable-line no-console
-            });
+    if (!navigator.serviceWorker) {
+        return;
     }
 
-    registerServiceWorker();
+    navigator.serviceWorker.register('/sw.js').then(() => {
+        console.log('Service Worker: registered'); // eslint-disable-line no-console
+    }).catch(err => {
+        console.log('Service Worker: registration failed ', err); // eslint-disable-line no-console
+    });
+}
 
-})();
+function updateRadioInput(id) {
+    if (id) {
+        document.getElementById(id).checked = true;
+    }
+}
+
+function initThemeSelector() {
+    const prefersDarkColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const themeToggle = document.querySelectorAll('.theme-form input[type="radio"]');
+    const themeLightId = 't-light';
+    const themeDarkId = 't-dark';
+
+    if (prefersDarkColorScheme) {
+        updateRadioInput(themeDarkId);
+    } else {
+        updateRadioInput(themeLightId);
+    }
+
+    themeToggle.forEach((toggle) => {
+        toggle.addEventListener('click', (e) => {
+            const id = e.target.value;
+
+            if (id === themeDarkId) {
+                document.documentElement.classList.remove(themeLightId);
+                document.documentElement.classList.add(themeDarkId);
+            } else {
+                document.documentElement.classList.remove(themeDarkId);
+                document.documentElement.classList.add(themeLightId);
+            }
+
+            updateRadioInput(id);
+        });
+    });
+}
+
+initThemeSelector();
+registerServiceWorker();
