2ff44bd619ec1c1d8408b36aa3f938c6f05d88a5
Alex Gibson <alexgibson@users.noreply.github.com>
2023-01-04 17:02:00 +0000

    Updates

--- index.aa69868b.js
+++
@@ -1,5 +0,0 @@
-if (window.navigator && navigator.serviceWorker) navigator.serviceWorker.getRegistrations().then((registrations)=>{
-    for (let registration of registrations)registration.unregister();
-});
-
-//# sourceMappingURL=index.aa69868b.js.map
