2c3c0b658f68cb0d2d705c090da6b5af52804eb3
Alex Gibson <alexgibson@users.noreply.github.com>
2025-03-05 10:55:16 +0000

    Add invert avatar

--- src/js/main.ts
+++ src/js/main.ts
@@ -9,4 +9,15 @@ function render(Ascii: string[]): void {
   console.log(chooseAscii(Ascii));
 }
 
+function invertAvatar(): void {
+  const avatar = document.querySelector(".avatar") as HTMLImageElement;
+
+  if (avatar) {
+    avatar.addEventListener("click", () => {
+      avatar.classList.toggle("is-inverted");
+    });
+  }
+}
+
 render(Ascii);
+invertAvatar();
